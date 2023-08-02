b53a18ae116cb698244719c04557f8d532c766cf
AGRIM ASWAL <124095607+agrimaswal@users.noreply.github.com>
2023-08-03 02:52:58 +0530

    Add files via upload

---
+++ parenthesischeck.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    static boolean ispar(String x) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : x.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+
